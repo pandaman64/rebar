950bd8548acc791b87ce5557327a15affabc908c
pandaman64 <kointosudesuyo@infoseek.jp>
2025-07-12 21:38:06 +0900

    Some more results

--- engines/lean/Main.lean
+++ engines/lean/Main.lean
@@ -197,7 +197,7 @@ def modelGrepCaptures (config : Config) : IO (Array Sample) :=
 
 def main (args : List String) : IO Unit := do
   if args.contains "--version" then
-    IO.println "v4.22.0-rc3 (5d40ebb)"
+    IO.println "v4.22.0-rc3 (f957590)"
     return
 
   let useBacktracker := args.contains "backtracker"
